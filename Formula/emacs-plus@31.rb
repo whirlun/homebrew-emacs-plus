aae95a70b857fe9e7f6e8fa06353bd80e6f1ef7d
whirlun <whirlun@yahoo.co.jp>
2026-05-14 23:07:01 -0700

    update sha256

--- Formula/emacs-plus@31.rb
+++ Formula/emacs-plus@31.rb
@@ -75,7 +75,7 @@ class EmacsPlusAT31 < EmacsBase
   local_patch "fix-ns-x-colors", sha: "9e5d3e26a8d388d3a000b697d582769645ca93ad597b4113744deba4b89a8b9e"
   local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
   local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
-  local_patch "glow-face-attribute", sha: "fd984a0defa2c398bb121ab7e5c35e6d954d720e2a60c5b100130ae4fa83ec89"
+  local_patch "glow-face-attribute", sha: "9eeb964ba0d4a26bc87094e2793a82f77d91facb78b2b391ef9273449deab0da"
   #
   # Install
   #
